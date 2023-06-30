1b3732fa4e580479a6e20bf97758d0dad9f5656c
Daniel <danozapmc@hotmail.com>
2023-06-30 16:51:09 -0400

    playlist bug fixed

--- src/js/main.js
+++ src/js/main.js
@@ -73,6 +73,15 @@ prevBtn.addEventListener("click", () => {
   prevMusic(); // prev song
 });
 
+musicAudio.addEventListener("ended", () => {
+    musicAudio.pause();
+    musicAudio.load();
+    musicAudio.currentTime = 0;
+    nextMusic();
+    
+    musicAudio.play();
+})
+
 //update progress bar
 musicAudio.addEventListener("timeupdate", (e) => {
   const currentTime = e.target.currentTime;
@@ -130,10 +139,83 @@ hideMusicBtn.addEventListener("click", () => {
   showMoreBtn.click();
 });
 
+let allMusic = [
+    {
+        name: "01 dE ENEro",
+        artist: "DUKI, Lucho SSJ",
+        src: "track-1"
+    },
+    {
+        name: "JEFES DEL SUDOESTE",
+        artist: "DUKI",
+        src: "track-2"
+    },
+    {
+        name: "RoCKSTAR 2.0",
+        artist: "DUKI, Jhayco",
+        src: "track-3"
+    },
+    {
+        name: "hARAkiRI",
+        artist: "DUKI, C.R.O",
+        src: "track-4"
+    },
+    {
+        name: "cONTRA><Mi",
+        artist: "DUKI, WE$T DUBAI",
+        src: "track-5"
+    },
+    {
+        name: "CSIpher (audio latino)",
+        artist: "DUKI, Akapellah, Neutro Shorty, Micro TDH",
+        src: "track-6"
+    },
+    {
+        name: "GiGi",
+        artist: "DUKI",
+        src: "track-7"
+    },
+    {
+        name: "don't lie",
+        artist: "DUKI, Quevedo",
+        src: "track-8"
+    },
+    {
+        name: "antes de perderte (OG VERSION)",
+        artist: "DUKI",
+        src: "track-9"
+    },
+    {
+        name: "uNO dOS",
+        artist: "DUKI",
+        src: "track-10"
+    },
+    {
+        name: "N.C.L.C",
+        artist: "DUKI",
+        src: "track-11"
+    },
+    {
+        name: "aPoLLo13",
+        artist: "DUKI",
+        src: "track-12"
+    },
+    {
+        name: "Ultimo Tren a Ameri",
+        artist: "DUKI",
+        src: "track-13"
+    },
+    {
+        name: "Buscando Ameri",
+        artist: "DUKI",
+        src: "track-14"
+    }
+]
+
 let ulTag = wrapper.querySelector("ul");
 
 for (let i = 0; i < allMusic.length; i++) {
-    let liTag = `<li li-index="${i + 1}">
+    let liTag = `<li li-index="${i}">
                     <div class="row">
                         <span>${allMusic[i].name}</span>
                         <p>${allMusic[i].artist}</p>
@@ -159,5 +241,31 @@ for (let i = 0; i < allMusic.length; i++) {
         liAudioDurationTag.setAttribute("t-duration", `${totalMin}:${totalSec}`);
     }
     );
-    
+}
+const allLiTag = ulTag.querySelectorAll("li");
+
+function playingNow() {
+    for(let j = 0; j < allLiTag.length; j++){
+        let audioTag = allLiTag[j].querySelector(".audio-duration");
+        if(allLiTag[j].classList.contains("playing")){
+            allLiTag[j].classList.remove("playing");
+            let adDuration = audioTag.getAttribute("t-duration");
+            audioTag.innerText = adDuration;
+        }
+        if(allLiTag[j].getAttribute("li-index") == musicIndex){
+            allLiTag[j].classList.add("playing");
+            audioTag.innerText = "Playing";
+        }
+        allLiTag[j].setAttribute("onclick", "clicked(this)");
+    }
+}
+
+playingNow();
+
+function clicked(element) {
+    let getLiIndex = element.getAttribute("li-index");
+    musicIndex = getLiIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingNow();
 }
